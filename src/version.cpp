48a8e665064a986c06515ac69e692a0a1c04e84e
c-jimenez <18682655+c-jimenez@users.noreply.github.com>
2022-02-09 23:17:11 +0100

    [make] Rework build system to obtain a single open-ocpp library in static and dynamic flavours

---
+++ src/version.cpp
@@ -0,0 +1,7 @@
+#include "Version.h"
+
+/** @brief Return the version string of the library */
+extern "C" const char* OPEN_OCPP_Version()
+{
+    return OPEN_OCPP_VERSION;
+}
--- src/version/Version.h
+++ src/version/Version.h
@@ -26,4 +26,7 @@ along with OpenOCPP. If not, see <http://www.gnu.org/licenses/>.
  */
 #define OPEN_OCPP_VERSION "0.5.0"
 
+/** @brief Return the version string of the library */
+extern "C" const char* OPEN_OCPP_Version();
+
 #endif // VERSION_H
