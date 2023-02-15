86cd73527d613fb9eb3ec854c90d4360620032f4
c-jimenez <18682655+c-jimenez@users.noreply.github.com>
2023-02-15 08:13:34 +0100

    [license] Use LGPLv2.1 or later

--- src/chargepoint/authent/AuthentCache.h
+++ src/chargepoint/authent/AuthentCache.h
@@ -4,8 +4,9 @@ Copyright (c) 2020 Cedric Jimenez
 This file is part of OpenOCPP.
 
 OpenOCPP is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License version 2.1
-as published by the Free Software Foundation.
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
 
 OpenOCPP is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
